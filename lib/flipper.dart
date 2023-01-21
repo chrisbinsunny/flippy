7293062039ae0743a651b23971da8bd9dd34f8df
Chrisbin Sunny <chrisbinsunny1@gmail.com>
2023-01-22 02:34:27 +0530

    Update flipper.dart

--- lib/flipper.dart
+++ lib/flipper.dart
@@ -2,4 +2,4 @@ library flipper;
 
 export 'flipper/dragFlipper.dart';
 export 'constants/parameters.dart';
-export 'controllers/flipperController.dart';
+export 'controllers/horizontal_flipper_controller.dart';
