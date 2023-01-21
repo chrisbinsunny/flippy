819afff7b2c74e3f2204de9418e526ebc96a2636
Chrisbin Sunny <chrisbinsunny1@gmail.com>
2023-01-22 02:34:37 +0530

    Update flipperController.dart

--- lib/controllers/flipperController.dart
+++ lib/controllers/flipperController.dart
@@ -14,6 +14,8 @@ class FlipperController extends ChangeNotifier {
 
   DragFlipperState? _state;
 
+  DragFlipperState? get state => _state;
+
   /// The internal widget state.
   /// Use only if you know what you're doing!
   set state(DragFlipperState? value) {
@@ -25,120 +27,5 @@ class FlipperController extends ChangeNotifier {
   final DragAxis dragAxis;
 
 
-
-
-  ///Flips the Flipper to the Left for one time.
-  ///Only works with [DragAxis.horizontal] and [DragAxis.both].
-  void flipLeft() {
-
-    assert(_state != null,
-    'Controller not attached to any Flipper. Seems that you forget to attach controller to Flipper.');
-    assert((dragAxis!=DragAxis.vertical),
-    'Cannot call flipLeft() when the dragAxis is set to DragAxis.vertical!'
-        '\nUse DragAxis.horizontal or DragAxis.both');
-
-    ///For DragAxis.both
-    ///Initialising the unused animation with the current value.
-    if(dragAxis==DragAxis.both) {
-      _state!.animationVertical =
-          AlwaysStoppedAnimation<double>(_state!.dragVertical);
-    }
-
-
-    _state!.animationHorizontal= Tween<double>(
-      begin: _state!.isFront?0:180,
-      end: _state!.isFront?180:360,
-    ).animate(_state!.animationController);
-    if(_state!.isInverted) {
-      _state!.animationController.reverse(from: 1);
-    }else{
-      _state!.animationController.forward(from: 0);
-    }
-
-  }
-
-  ///Flips the Flipper to the Right for one time.
-  ///Only works with [DragAxis.horizontal] and [DragAxis.both].
-  void flipRight(){
-
-    assert(_state != null,
-    'Controller not attached to any Flipper. Seems that you forget to attach controller to Flipper.');
-    assert((dragAxis!=DragAxis.vertical),
-    'Cannot call flipRight() when the dragAxis is set to DragAxis.vertical!'
-        '\nUse DragAxis.horizontal or DragAxis.both');
-
-    ///For DragAxis.both
-    ///Initialising the unused animation with the current value.
-    if(dragAxis==DragAxis.both) {
-    _state!.animationVertical= AlwaysStoppedAnimation<double>(_state!.dragVertical);
-    }
-
-    _state!.animationHorizontal= Tween<double>(
-      begin: _state!.isFront?360:180,
-      end: _state!.isFront?180:0,
-    ).animate(_state!.animationController);
-
-    if(_state!.isInverted) {
-      _state!.animationController.reverse(from: 1);
-    }else{
-      _state!.animationController.forward(from: 0);
-    }
-
-  }
-
-  ///Flips the Flipper to the Bottom for one time.
-  ///Only works with [DragAxis.vertical] and [DragAxis.both].
-  void flipDown(){
-
-    assert(_state != null,
-    'Controller not attached to any Flipper. Seems that you forget to attach controller to Flipper.');
-    assert((dragAxis!=DragAxis.horizontal),
-    'Cannot call flipDown() when the dragAxis is set to DragAxis.horizontal!'
-        '\nUse DragAxis.vertical or DragAxis.both');
-
-
-    ///For DragAxis.both
-    ///Initialising the unused animation with the current value.
-    if(dragAxis==DragAxis.both) {
-      _state!.animationHorizontal= AlwaysStoppedAnimation<double>(_state!.dragHorizontal);
-    }
-
-    _state!.animationVertical= Tween<double>(
-      begin: _state!.isInverted?180:0,
-      end: _state!.isInverted?360:180,
-    ).animate(_state!.animationController);
-
-    _state!.animationController.forward(from: 0);
-
-  }
-
-  ///Flips the Flipper to the Top for one time.
-  ///Only works with [DragAxis.vertical] and [DragAxis.both].
-  void flipUp(){
-
-    assert(_state != null,
-    'Controller not attached to any Flipper. Seems that you forget to attach controller to Flipper.');
-    assert((dragAxis!=DragAxis.horizontal),
-    'Cannot call flipUp() when the dragAxis is set to DragAxis.horizontal!'
-        '\nUse DragAxis.vertical or DragAxis.both');
-
-
-    ///For DragAxis.both
-    ///Initialising the unused animation with the current value.
-
-    if(dragAxis==DragAxis.both) {
-    _state!.animationHorizontal= AlwaysStoppedAnimation<double>(_state!.dragHorizontal);
-    }
-
-
-    
-    _state!.animationVertical= Tween<double>(
-      begin: _state!.isInverted?180:360,
-      end:   _state!.isInverted?0:180,
-    ).animate(_state!.animationController);
-
-    _state!.animationController.forward(from: 0);
-
-  }
 
 }
