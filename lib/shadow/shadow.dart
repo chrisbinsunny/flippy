61b8e574eef0f6066936b13cbfe8ad20a4c35019
Chrisbin Sunny <chrisbinsunny1@gmail.com>
2023-01-22 14:14:17 +0530

    Update shadow.dart

--- lib/shadow/shadow.dart
+++ lib/shadow/shadow.dart
@@ -1,13 +1,42 @@
 
+import 'package:flipper/controllers/flipperController.dart';
 import 'package:flutter/material.dart';
 
+import 'blurPainter.dart';
+
 class FlipperShadow extends StatelessWidget {
-  const FlipperShadow({Key? key}) : super(key: key);
+  const FlipperShadow({
+    Key? key,
+    required this.controller,
+    this.blurSigma=11,
+    this. height= 7,
+    this.color=const Color(0xff8a8a8a),
+    this.spacing= 30
+
+  }) : super(key: key);
+
+  final FlipperController controller;
+  final double blurSigma, height, spacing;
+  final Color color;
 
   @override
   Widget build(BuildContext context) {
-    return CustomPaint(
-        foregroundPainter:
-        CircleBlurPainter(blurSigma: 11, width: getWidth())),;
+    return Padding(
+      padding: EdgeInsets.only(
+        top: spacing
+      ),
+      child: CustomPaint(
+
+        painter: FlipperShadowPainter(
+          blurSigma: blurSigma,
+          width: controller.getWidth(),
+          color: color,
+          height:height,
+        ),
+          ),
+    );
   }
+
+
+
 }
