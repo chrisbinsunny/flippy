1cd3ecb97ef2ee708cc70a180f4261a2dadf1dac
Chrisbin Sunny <chrisbinsunny1@gmail.com>
2023-01-22 13:00:37 +0530

    Update main.dart

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -30,7 +30,9 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
-  FlipperController controller= HorizontalFlipperController();
+  FlipperController controller= FlipperController(
+    dragAxis: DragAxis.horizontal,
+  );
 
   @override
   Widget build(BuildContext context) {
