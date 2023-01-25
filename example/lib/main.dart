0cc798a7cebd8aa3c237b912d12c54753202ec35
Chrisbin Sunny <chrisbinsunny1@gmail.com>
2023-01-25 05:48:21 +0530

    Update main.dart

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -8,11 +8,10 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flippy Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -34,145 +33,26 @@ class _MyHomePageState extends State<MyHomePage> {
     dragAxis: DragAxis.horizontal,
   );
 
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Flipper Example"),
+        title: const Text("Flippy Example"),
       ),
       body: Center(
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
-            Flipper(
+            DragFlipper(
               controller: controller,
               //borderRadius: const BorderRadius.all(Radius.circular(8.0)),
-              front: LayoutBuilder(
-                  builder: (p0, constraints) {
-                    return Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        SizedBox(
-                            height: constraints.maxHeight*0.1
-                        ),
-                        const SizedBox(
-                          height: 10,
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 33
-                          ),
-                          child: FittedBox(
-                            fit: BoxFit.scaleDown,
-
-                            child: RichText(
-                              text: const TextSpan(
-                                  text: "Chrisbin\n",
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.w800,
-                                      color: Color(0xff5dc8f8),
-                                      fontSize: 32,
-                                      fontFamily: "Gilroy",
-                                      letterSpacing: 1.5
-                                  ),
-                                  children: [
-                                    TextSpan(
-                                      text: "Sunny",
-                                      style: TextStyle(
-                                          fontWeight: FontWeight.w600,
-                                          color: Color(0xff065a9d),
-                                          fontSize: 35,
-                                          height: 0.9,
-                                          letterSpacing: 0.5
-
-                                      ),
-                                    ),
-                                  ]
-                              ),
-                              textAlign: TextAlign.center,
-                            ),
-                          ),
-                        ),
-
-                      ],
-                    );}
-              ),
-              back: LayoutBuilder(
-
-                builder: (p0, constraints) {
-                  return Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const SizedBox(
-                        height: 30,
-                      ),
-                      SizedBox(
-                        width: constraints.maxWidth*0.65,
-                        child: const FittedBox(
-                          child: Text(" Chrisbin Sunny",
-                            style: TextStyle(
-                                fontSize: 24,
-                                color: Colors.white,
-                                fontWeight: FontWeight.w600
-                            ),
-                          ),
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 2,
-                      ),
-                      SizedBox(
-                        width: constraints.maxWidth*0.6,
-                        child: const FittedBox(
-                          child: Text("  App Developer | Speaker",
-                            style: TextStyle(
-                                fontSize: 15,
-                                color: Colors.white,
-                                fontWeight: FontWeight.w200
-                            ),
-                          ),
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 30,
-                      ),
-                      SizedBox(
-                        width: constraints.maxWidth*0.55,
-                        child: const FittedBox(
-                          child: Text(
-                            " (IND)  +91 83300 70512",
-                            style: TextStyle(
-                                fontSize: 15,
-                                color: Colors.white,
-                                fontWeight: FontWeight.w300
-                            ),
-                          ),
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 2,
-                      ),
-                      SizedBox(
-                        width: constraints.maxWidth*0.65,
-                        child: const FittedBox(
-                          child: Text(
-                            " chrisbinofficial@gmail.com",
-                            style: TextStyle(
-                                fontSize: 15,
-                                color: Colors.white,
-                                fontFamily: "Gilroy",
-                                fontWeight: FontWeight.w300
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  );
-                },),
+              front: const FrontWidget(),
+              back: const BackWidget(),
+              showShadow: true
             ),
-            FlipperShadow(
-              controller: controller,
-              blurSigma: 10,
+            const SizedBox(
+              height: 40,
             ),
             Row(
               mainAxisSize: MainAxisSize.min,
@@ -183,7 +63,7 @@ class _MyHomePageState extends State<MyHomePage> {
                       controller.flipLeft();
 
                     },
-                    icon: Icon(
+                    icon: const Icon(
                       Icons.arrow_circle_left_outlined,
                       color: Colors.red,
                       size: 30,
@@ -193,7 +73,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     onPressed: (){
                       controller.flipRight();
                     },
-                    icon: Icon(
+                    icon: const Icon(
                       Icons.arrow_circle_right_outlined,
                       color: Colors.red,
                       size: 30,
@@ -208,7 +88,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     onPressed: (){
                       controller.flipDown();
                     },
-                    icon: Icon(
+                    icon: const Icon(
                       Icons.arrow_circle_down_outlined,
                       color: Colors.red,
                       size: 30,
@@ -219,7 +99,7 @@ class _MyHomePageState extends State<MyHomePage> {
                       controller.flipUp();
 
                     },
-                    icon: Icon(
+                    icon: const Icon(
                       Icons.arrow_circle_up_outlined,
                       color: Colors.red,
                       size: 30,
@@ -227,9 +107,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 )
               ],
             ),
-            SizedBox(
-              height: 20,
-            )
+
           ],
         ),
       ),
@@ -238,3 +116,140 @@ class _MyHomePageState extends State<MyHomePage> {
 }
 
 
+class FrontWidget extends StatelessWidget {
+  const FrontWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+        builder: (p0, constraints) {
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                  height: constraints.maxHeight*0.1
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                    horizontal: 33
+                ),
+                child: FittedBox(
+                  fit: BoxFit.scaleDown,
+
+                  child: RichText(
+                    text: const TextSpan(
+                        text: "Chrisbin\n",
+                        style: TextStyle(
+                            fontWeight: FontWeight.w800,
+                            color: Color(0xff5dc8f8),
+                            fontSize: 32,
+                            fontFamily: "Gilroy",
+                            letterSpacing: 1.5
+                        ),
+                        children: [
+                          TextSpan(
+                            text: "Sunny",
+                            style: TextStyle(
+                                fontWeight: FontWeight.w600,
+                                color: Color(0xff065a9d),
+                                fontSize: 35,
+                                height: 0.9,
+                                letterSpacing: 0.5
+
+                            ),
+                          ),
+                        ]
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+
+            ],
+          );}
+    );
+  }
+}
+
+class BackWidget extends StatelessWidget {
+  const BackWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+
+      builder: (p0, constraints) {
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(
+              height: 30,
+            ),
+            SizedBox(
+              width: constraints.maxWidth*0.65,
+              child: const FittedBox(
+                child: Text(" Chrisbin Sunny",
+                  style: TextStyle(
+                      fontSize: 24,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w600
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 2,
+            ),
+            SizedBox(
+              width: constraints.maxWidth*0.6,
+              child: const FittedBox(
+                child: Text("  App Developer | Speaker",
+                  style: TextStyle(
+                      fontSize: 15,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w200
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            SizedBox(
+              width: constraints.maxWidth*0.55,
+              child: const FittedBox(
+                child: Text(
+                  " (IND)  +91 83300 70512",
+                  style: TextStyle(
+                      fontSize: 15,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w300
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 2,
+            ),
+            SizedBox(
+              width: constraints.maxWidth*0.65,
+              child: const FittedBox(
+                child: Text(
+                  " chrisbinofficial@gmail.com",
+                  style: TextStyle(
+                      fontSize: 15,
+                      color: Colors.white,
+                      fontFamily: "Gilroy",
+                      fontWeight: FontWeight.w300
+                  ),
+                ),
+              ),
+            ),
+          ],
+        );
+      },);
+  }
+}
