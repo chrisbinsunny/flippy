05284cc545daf712e2b0ebf21cc91767498f9e56
Chrisbin Sunny <chrisbinsunny1@gmail.com>
2023-01-25 18:56:13 +0530

    Update main.dart

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -56,7 +56,9 @@ class _MyHomePageState extends State<MyHomePage> {
                   Color(0xff5f5eda),
                 ]
               ),
-              padding: EdgeInsets.zero,
+              padding: const EdgeInsets.symmetric(
+                vertical: 8,
+              ),
               height: 210,
               showShadow: true
             ),
@@ -130,124 +132,127 @@ class FrontWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      mainAxisAlignment: MainAxisAlignment.spaceAround,
-      children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: const [
-            Text(
-              "PLATINUM",
-              style: TextStyle(
-                fontSize: 15,
-                color: Colors.white
-              ),
-            ),
-            Text(
-              "MOCKUP BANK",
-              style: TextStyle(
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 12),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: const [
+              Text(
+                "PLATINUM",
+                style: TextStyle(
                   fontSize: 15,
                   color: Colors.white
+                ),
               ),
-            ),
-          ],
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            Container(
-              width: 70,
-              height: 43,
-              decoration: BoxDecoration(
-                color: const Color(0xffe0b056),
-                borderRadius: BorderRadius.circular(8)
+              Text(
+                "MOCKUP BANK",
+                style: TextStyle(
+                    fontSize: 15,
+                    color: Colors.white
+                ),
               ),
-            ),
-            const Text(
-              "CREDIT CARD",
-              style: TextStyle(
-                  fontSize: 15,
-                  color: Colors.white
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Container(
+                width: 70,
+                height: 43,
+                decoration: BoxDecoration(
+                  color: const Color(0xffe0b056),
+                  borderRadius: BorderRadius.circular(8)
+                ),
               ),
-            ),
-          ],
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: const [
-            Text(
-              "0603",
-              style: TextStyle(
-                  fontSize: 27,
-                  color: Colors.white,
-                fontWeight: FontWeight.bold
+              const Text(
+                "CREDIT CARD",
+                style: TextStyle(
+                    fontSize: 15,
+                    color: Colors.white
+                ),
               ),
-            ),
-            Text(
-              "9020",
-              style: TextStyle(
-                  fontSize: 27,
-                  color: Colors.white,
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: const [
+              Text(
+                "0603",
+                style: TextStyle(
+                    fontSize: 27,
+                    color: Colors.white,
                   fontWeight: FontWeight.bold
+                ),
               ),
-            ),Text(
-              "0714",
-              style: TextStyle(
-                  fontSize: 27,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold
+              Text(
+                "9020",
+                style: TextStyle(
+                    fontSize: 27,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold
+                ),
+              ),Text(
+                "0714",
+                style: TextStyle(
+                    fontSize: 27,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold
+                ),
+              ),Text(
+                "1234",
+                style: TextStyle(
+                    fontSize: 27,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold
+                ),
               ),
-            ),Text(
-              "1234",
-              style: TextStyle(
-                  fontSize: 27,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold
-              ),
-            ),
-          ],
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text(
-                  "JANE DOE",
-                  style: TextStyle(
-                      fontSize: 17,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    "JANE DOE",
+                    style: TextStyle(
+                        fontSize: 17,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold
+                    ),
                   ),
-                ),
-                const SizedBox(
-                  height: 3,
-                ),
-                const Text(
-                  "VALID THRU 05/28",
-                  style: TextStyle(
-                      fontSize: 10,
-                      color: Colors.white,
+                  const SizedBox(
+                    height: 3,
                   ),
-                ),
-              ],
-            ),
+                  const Text(
+                    "VALID THRU 05/28",
+                    style: TextStyle(
+                        fontSize: 10,
+                        color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
 
-            const RotatedBox(
-              quarterTurns: 1,
-              child: Icon(
-                Icons.wifi,
-                color: Colors.white,
-                size: 30,
+              const RotatedBox(
+                quarterTurns: 1,
+                child: Icon(
+                  Icons.wifi,
+                  color: Colors.white,
+                  size: 30,
+                ),
               ),
-            ),
-          ],
-        ),
-      ],
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
@@ -264,106 +269,59 @@ class BackWidget extends StatelessWidget {
         Container(
           width: double.infinity,
           height: 43,
-          decoration: BoxDecoration(
-              color: const Color(0xffe0b056),
-              borderRadius: BorderRadius.circular(8)
+          decoration: const BoxDecoration(
+              color: Color(0xff1f1e1e),
           ),
         ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: const [
-            Text(
-              "0603",
-              style: TextStyle(
-                  fontSize: 27,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold
-              ),
+        Container(
+          width: 180,
+          height: 37,
+          alignment: Alignment.centerRight,
+          decoration: const BoxDecoration(
+            color: Colors.white70,
+          ),
+          child: Text(
+            "0210  ",
+            style: TextStyle(
+              fontSize: 18
             ),
-            Text(
-              "9020",
-              style: TextStyle(
-                  fontSize: 27,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold
-              ),
-            ),Text(
-              "0714",
-              style: TextStyle(
-                  fontSize: 27,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold
-              ),
-            ),Text(
-              "1234",
-              style: TextStyle(
-                  fontSize: 27,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold
-              ),
-            ),
-          ],
+          )
         ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text(
-                  "JANE DOE",
-                  style: TextStyle(
-                      fontSize: 17,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold
-                  ),
-                ),
-                const SizedBox(
-                  height: 3,
-                ),
-                const Text(
-                  "VALID THRU 05/28",
-                  style: TextStyle(
-                    fontSize: 10,
-                    color: Colors.white,
-                  ),
-                ),
-              ],
-            ),
-
-            const RotatedBox(
-              quarterTurns: 1,
-              child: Icon(
-                Icons.wifi,
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 12),
+          child: Text(
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+            style: TextStyle(
+                fontSize: 13,
                 color: Colors.white,
-                size: 30,
-              ),
             ),
-          ],
+          ),
         ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: const [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 12),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: const [
 
-            Text(
-              "MOCKUP BANK",
-              style: TextStyle(
-                  fontSize: 15,
-                  color: Colors.white,
+              Text(
+                "MOCKUP BANK",
+                style: TextStyle(
+                    fontSize: 15,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold
+
+                ),
+              ),
+              Text(
+                "CREDIT CARD",
+                style: TextStyle(
+                    fontSize: 15,
+                    color: Colors.white,
                   fontWeight: FontWeight.bold
-
+                ),
               ),
-            ),
-            Text(
-              "CREDIT CARD",
-              style: TextStyle(
-                  fontSize: 15,
-                  color: Colors.white,
-                fontWeight: FontWeight.bold
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ],
     );
